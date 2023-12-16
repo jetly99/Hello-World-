22e4cd9d2ee0186a24f436d191e883277f940c6d
po <110640640+jetly99@users.noreply.github.com>
2023-12-16 10:37:15 +0900

    index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Hello Wolrd!</title>  
+    </head>
+    <body>
+       <h1>hello world!</h1> <br>
+       <p>Welcome to my home page</p> </br>
+        <!-- HTML code for the subscribe button -->
+    <div class="g-ytsubscribe" data-channelid=
+      "UC_x5XG1OV2P6uZZ5FSM9Ttw" data-layout="full" data-count="default">
+     </div>
+       <!-- Script to load the YouTube API -->
+    <script 
+      src="https://apis.google.com/js/platform.js">
+     </script>
+    </body>
+</html>
